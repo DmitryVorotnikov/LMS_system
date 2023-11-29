72df0491965fe7afa8aa3e6aa6bbe12ff169dd8f
dmitryvorot <vorotnikov7009@mail.ru>
2023-11-29 12:28:23 +0800

    Added Course Model ViewSet, Lesson Model Generics, and Serializer with Routing

--- education/urls.py
+++ education/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from education.apps import EducationConfig
+from education.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = EducationConfig.name
 
-urlpatterns = [
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='courses')
 
+urlpatterns = [
+    path('lessons/create/', LessonCreateAPIView.as_view(), name='lessons_create'),
+    path('lessons/', LessonListAPIView.as_view(), name='lessons_list'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lessons_get'),
+    path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lessons_update'),
+    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lessons_delete'),
 ]
+
+urlpatterns += router.urls
