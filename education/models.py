dfeba1ce66b6fd663ca16df4ced57898efadfc95
dmitryvorot <vorotnikov7009@mail.ru>
2023-11-29 11:56:54 +0800

    Added Course and Lesson Models

--- education/models.py
+++ education/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    preview = models.ImageField(upload_to='education/preview/', verbose_name='Превью', **NULLABLE)
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ('id',)
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey('Course', on_delete=models.CASCADE, verbose_name='Курс')
+    name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    preview = models.ImageField(upload_to='education/preview/', verbose_name='Превью', **NULLABLE)
+    link_to_video = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+        ordering = ('id',)
