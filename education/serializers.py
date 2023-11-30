435b4422e952b0a242574ad08da898d06c7c4c72
dmitryvorot <vorotnikov7009@mail.ru>
2023-12-01 00:32:51 +0800

    Added lesson_count to Course serializer

--- education/serializers.py
+++ education/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from education.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    # Добавлено поле для количества уроков по курсу.
+    def get_lesson_count(self, instance):
+        return Lesson.objects.filter(course_id=instance.id).count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
