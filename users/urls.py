ccbb7d3c82dd80d861b09c3591197963d52a76fe
dmitryvorot <vorotnikov7009@mail.ru>
2023-11-30 11:06:04 +0800

    Refactored User model view to Generic classes

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserViewSet
+from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView
 
 app_name = UsersConfig.name
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('create/', UserCreateAPIView.as_view(), name='users_create'),
+    path('', UserListAPIView.as_view(), name='users_list'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='users_get'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='users_update'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='users_delete'),
+]
