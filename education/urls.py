89f85fc7a442bdd4a0218236bd4df90bdc760d39
dmitryvorot <vorotnikov7009@mail.ru>
2023-12-03 21:48:15 +0800

    Added URL Routing for Subscriptions

--- education/urls.py
+++ education/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 
 from education.apps import EducationConfig
 from education.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, SubscriptionCreateAPIView, SubscriptionListAPIView, \
+    SubscriptionDestroyAPIView
 
 app_name = EducationConfig.name
 
@@ -11,11 +12,17 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
+    # URLs уроков:
     path('lessons/create/', LessonCreateAPIView.as_view(), name='lessons_create'),
     path('lessons/', LessonListAPIView.as_view(), name='lessons_list'),
     path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lessons_get'),
     path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lessons_update'),
     path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lessons_delete'),
+
+    # URLs подписок:
+    path('subs/create/', SubscriptionCreateAPIView.as_view(), name='subs_create'),
+    path('subs/', SubscriptionListAPIView.as_view(), name='subs_list'), # Надо удалить потом !!!!!!!!!!!!!!!!!
+    path('subs/delete/<int:pk>/', SubscriptionDestroyAPIView.as_view(), name='subs_delete'),
 ]
 
 urlpatterns += router.urls
