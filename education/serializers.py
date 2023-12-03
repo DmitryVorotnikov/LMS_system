9e21088be516f0954ec6404893e0b85dc1b249b1
dmitryvorot <vorotnikov7009@mail.ru>
2023-12-03 21:55:00 +0800

    Added subscription_status field to Course serializer

--- education/serializers.py
+++ education/serializers.py
@@ -20,13 +20,24 @@ class LessonSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    # Поля для отображения количества уроков.
     lessons_count = serializers.IntegerField(source='lesson_set.count', default=0, read_only=True)
     # Поле для отображения уроков в get-запрос на курсы.
     lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
+    # Поля для отображения статуса подписки.
+    subscription_status = SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_subscription_status(self, instance):
+        if instance.subscription_set.exists():
+            return f'Вы подписаны'
+
+        return None
+
+
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
