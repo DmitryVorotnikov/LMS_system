aa2e422ff0d0f011bda20166b08a45d01b2162c7
dmitryvorot <vorotnikov7009@mail.ru>
2023-12-11 04:45:14 +0800

    Updated perform_create method for Payment creation

--- payments/urls.py
+++ payments/urls.py
@@ -6,13 +6,10 @@ from payments.views import PaymentCreateAPIView, PaymentListAPIView, PaymentRetr
 
 app_name = PaymentsConfig.name
 
-
 urlpatterns = [
     path('create/', PaymentCreateAPIView.as_view(), name='payments_create'),
     path('', PaymentListAPIView.as_view(), name='payments_list'),
     path('<int:pk>/', PaymentRetrieveAPIView.as_view(), name='payments_get'),
-    path('update/<int:pk>/', PaymentUpdateAPIView.as_view(), name='payments_update'),
-    path('delete/<int:pk>/', PaymentDestroyAPIView.as_view(), name='payments_delete'),
+    # path('update/<int:pk>/', PaymentUpdateAPIView.as_view(), name='payments_update'),
+    # path('delete/<int:pk>/', PaymentDestroyAPIView.as_view(), name='payments_delete'),
 ]
-
-
--- payments/views.py
+++ payments/views.py
@@ -4,26 +4,51 @@ from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
 from payments.models import Payment
-from payments.serializers import PaymentSerializer
+from payments.serializers import PaymentSerializer, PaymentRetrieveSerializer
+from payments.services import get_link_for_payment
 
 
 class PaymentCreateAPIView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = PaymentSerializer
 
+    def perform_create(self, serializer):
+        new_payment = serializer.save()
+
+        # Присваиваем текущего пользователя как создателя платежа.
+        new_payment.creator = self.request.user
+
+        paid_course = self.request.data.get('paid_course')
+        payment_amount = self.request.data.get('payment_amount')
+        # Создаем сессию платежа и сохраняем id сессии и ссылку на платеж.
+        link_for_payment, payment_session_id = get_link_for_payment(paid_course=paid_course,
+                                                                    payment_amount=payment_amount)
+
+        new_payment.link_for_payment = link_for_payment
+        new_payment.payment_session_id = payment_session_id
+
+        new_payment.save()
+
 
 class PaymentListAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = PaymentSerializer
-    queryset = Payment.objects.all()
+    serializer_class = PaymentRetrieveSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     ordering_fields = ['date_of_payment']
     filterset_fields = ('paid_course', 'payment_type',)
 
+    def get_queryset(self):
+        queryset = Payment.objects.all()
+        # Обычному пользователю в queryset указываем его платежи.
+        if not self.request.user.is_staff:
+            user_id = self.request.user.id
+            queryset = queryset.filter(creator=user_id)
+        return queryset
+
 
 class PaymentRetrieveAPIView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = PaymentSerializer
+    serializer_class = PaymentRetrieveSerializer
     queryset = Payment.objects.all()
 
 
