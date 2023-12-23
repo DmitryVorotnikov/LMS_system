837c0a4fa260d5814caba5591ae5b9eba02ec758
dmitryvorot <vorotnikov7009@mail.ru>
2023-12-23 12:58:34 +0800

    Added password hashing functions in services.py
    Implemented password hashing functions in services.py to enhance the security of storing user passwords. Password hashing before saving to the database is now done by calling these functions within serializers, enhancing modularity and security of the system

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from payments.serializers import PaymentSerializer
 from users.models import User
+from users.services import password_hashing_on_creation, password_hashing_on_update
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,6 +12,14 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('password', 'first_name', 'last_name', 'email', 'phone_number', 'city', 'avatar', 'payments',)
 
+    def create(self, validated_data):
+        password_hashing_on_creation(validated_data)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        password_hashing_on_update(validated_data)
+        return super().update(instance, validated_data)
+
 
 class UserForAdminSerializer(serializers.ModelSerializer):
     payments = PaymentSerializer(source='payment_set', many=True, read_only=True)
@@ -18,6 +27,14 @@ class UserForAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        password_hashing_on_creation(validated_data)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        password_hashing_on_update(validated_data)
+        return super().update(instance, validated_data)
 
 
 class UserForUserSerializer(serializers.ModelSerializer):
--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.hashers import make_password
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
@@ -8,17 +7,6 @@ from users.serializers import UserSerializer, UserForAdminSerializer, UserForUse
 
 
 class UserCreateAPIView(generics.CreateAPIView):
-
-    # Метод для того чтобы не записывать пароль в чистом виде в БД, а сначала хешировать его.
-    def perform_create(self, serializer):
-        # Получение пароля из запроса.
-        password = self.request.data.get('password')
-
-        # Хеширование пароля.
-        hashed_password = make_password(password)
-
-        # Установка хешированного пароля в сериализатор перед сохранением в БД.
-        serializer.save(password=hashed_password)
 
     def get_serializer_class(self):
         if self.request.user.is_staff:
@@ -59,18 +47,6 @@ class UserUpdateAPIView(generics.UpdateAPIView):
     permission_classes = [IsAuthenticated, UserUpdateDeletePermission]
     serializer_class = UserSerializer
     queryset = User.objects.all()
-
-    # Метод для того чтобы не записывать пароль в чистом виде в БД, а сначала хешировать его.
-    def perform_update(self, serializer):
-        # Получение пароля из запроса.
-        password = self.request.data.get('password')
-
-        # Хеширование пароля, если он был изменен.
-        if password:
-            hashed_password = make_password(password)
-            serializer.save(password=hashed_password)
-        else:
-            serializer.save()
 
     def get_serializer_class(self):
         if self.request.user.is_staff:
