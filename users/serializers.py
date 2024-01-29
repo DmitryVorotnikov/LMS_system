0faed174e135dfc506a741076b03b168a815d13c
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-01-30 06:51:24 +0800

    Optimized password hashing function

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from payments.serializers import PaymentSerializer
 from users.models import User
-from users.services import password_hashing_on_creation, password_hashing_on_update
+from users.services import password_hashing
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,11 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('password', 'first_name', 'last_name', 'email', 'phone_number', 'city', 'avatar', 'payments',)
 
     def create(self, validated_data):
-        password_hashing_on_creation(validated_data)
+        password_hashing(validated_data)
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        password_hashing_on_update(validated_data)
+        password_hashing(validated_data)
         return super().update(instance, validated_data)
 
 
@@ -29,15 +29,15 @@ class UserForAdminSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        password_hashing_on_creation(validated_data)
+        password_hashing(validated_data)
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        password_hashing_on_update(validated_data)
+        password_hashing(validated_data)
         return super().update(instance, validated_data)
 
 
 class UserForUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'email', 'phone_number', 'city', 'avatar',)
+        fields = ('password', 'first_name', 'email', 'phone_number', 'city', 'avatar',)
